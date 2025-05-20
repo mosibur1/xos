f1497a0044809501e106a2abe2771acda79b20f0
Mosibur Paik <51851200+mosibur1@users.noreply.github.com>
2025-05-20 22:18:38 +0530

    Update banner.js

--- core/banner.js
+++ core/banner.js
@@ -1,6 +1,6 @@
 const colors = require("colors");
 function showBanner() {
-  console.log(colors.yellow("Tool được phát triển bởi nhóm tele Airdrop Hunter Siêu Tốc (https://t.me/airdrophuntersieutoc)"));
+  console.log(colors.yellow("Script By MRPTech)"));
 }
 
 module.exports = { showBanner };
